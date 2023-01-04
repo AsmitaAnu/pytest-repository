7c35c2e54e34568a2735de8c645c1f08526db182
Asmita Bisoi <96170504+AsmitaAnu@users.noreply.github.com>
2023-01-04 10:15:16 +0530

    Update test_repo.py

--- test_repo.py
+++ test_repo.py
@@ -1,30 +1,37 @@
 import requests
+def git_create_repo():
 
-def git_repo():
     personal_access_token = "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"
     api_base_url = "https://api.github.com"
 
     data = {
-        "name": "pytest-repository",
-        "description": "This is my new repository",
-        "private": False
+    "name": "pytest-repository",
+    "description": "This is my new repository",
+    "private": False
     }
-    response = requests.post(f"{api_base_url}/user/repos", json=data,
-                             headers={"Authorization": f"Bearer {personal_access_token}"})
+
+    response = requests.post(f"{api_base_url}/user/repos", json=data, headers={"Authorization": f"Bearer {personal_access_token}"})
+
     return response.status_code
 
-def test_git_repo():
-    #value = git_regsiter()
-    #assert value == 201
-    assert git_repo() == 201
+    
+
+def test_git_create_repo():
+    value = git_create_repo()
+    assert value == 201
+
 
 def git_login():
-    username = 'AsmitaAnu'
-    token = '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'
 
-    login = requests.get('https://api.github.com/search/repositories?q=github+api', auth=(username, token))
-    return login
-    print('login')
+    personal_access_token = "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"
+    api_base_url = "https://api.github.com"
 
-#def test_git_login():
- #   assert git_login() == 201
+    response = requests.get(f"{api_base_url}/user/repos", headers={"Authorization": f"Bearer {personal_access_token}"})
+
+    #return response.status_code
+    return response.status_code
+
+    
+def test_git_login():
+    login = git_login()
+    assert login == 200
